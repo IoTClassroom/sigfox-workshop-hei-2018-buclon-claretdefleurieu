15f92fcc31c4e3f28a0795cb85cfa98d2889ee4e
EBuclon <32641286+EBuclon@users.noreply.github.com>
2018-02-12 10:28:46 +0100

    Add files via upload

---
+++ Capteur/Capteur.ino
@@ -0,0 +1,20 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+   Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  int sensorValue = analogRead(A0);
+  Serial.print("light: ");
+  Serial.println(sensorValue);
+
+  int percentage = map(sensorValue, 0, 1023, 0, 100);
+
+  Serial.print("light percentage: ");
+  Serial.print(percentage);
+  Serial.println(" % ");
+
+  delay(1000);
+}
